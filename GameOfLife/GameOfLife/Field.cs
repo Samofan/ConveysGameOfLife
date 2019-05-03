a91e28159a9fb5d6568a2e3a77dcc6f12b2d7050
Flo D <45032794+Samofan@users.noreply.github.com>
2019-05-03 12:56:56 +0100

    Add files via upload

---
+++ GameOfLife/GameOfLife/Field.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Field
+    {
+        Boolean isAlive;
+
+        public Field(Boolean living)
+        {
+            isAlive = living;
+        }
+
+        public void setAlive(Boolean living)
+        {
+            isAlive = living;
+        }
+
+        public Boolean getStatus() 
+        {
+            return isAlive;
+        }
+    }
+    
+}
---
+++ GameOfLife/GameOfLife/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Utils
+    {
+        public static void GenerateFields(int quantity)
+        {
+            Field[] fieldArray = new Field[quantity];
+            for (int i = 0; i < quantity; i++)
+            {
+                Random random = new Random();
+                int randomField = random.Next(0, 1);
+                if (randomField == 0)
+                {
+                    Field field = new Field(false);
+                    fieldArray[i] = field;
+                }
+                else
+                {
+                    Field field = new Field(true);
+                    fieldArray[i] = field;
+                }
+            }
+        }
+
+        public static void printFields(Field[] fieldArray)
+        {
+          Field[] fieldFeatures = new Field[];
+        }
+    }
+}
