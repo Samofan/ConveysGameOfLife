9d725cff52d6873e7485bccfbea540a13df1c3d1
Flo D <45032794+Samofan@users.noreply.github.com>
2019-05-09 20:33:53 +0200

    Update Field.cs

--- GameOfLife/GameOfLife/Field.cs
+++ GameOfLife/GameOfLife/Field.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +15,36 @@ namespace GameOfLife
             isAlive = living;
         }
 
-        public void setAlive(Boolean living)
+        public void SetAlive(Boolean living)
         {
             isAlive = living;
         }
 
-        public Boolean getStatus() 
+        public Boolean GetStatus() 
         {
             return isAlive;
+        }
+
+        public static Field[] GenerateFields(int quantity)
+        {
+            Field[] fieldArray = new Field[quantity];
+            Random random = new Random();
+
+            for (int i = 0; i < quantity; i++)
+            {
+                int randomFieldStatus = random.Next(0, 2);
+                if (randomFieldStatus == 0)
+                {
+                    Field field = new Field(false);
+                    fieldArray[i] = field;
+                }
+                else
+                {
+                    Field field = new Field(true);
+                    fieldArray[i] = field;
+                }
+            }
+            return fieldArray;
         }
     }
     
